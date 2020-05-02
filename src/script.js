df06e9e235b0f9064d2bb14765f7d15e195ee22d
Vincent Palmer <vpalmergraphics@gmail.com>
2020-05-01 20:39:36 -0400

    prod url

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 document.getElementById('search-result').innerText = 'Use the searchbar above to get results.';
 
 async function tryIt() {
-	// http://swapi.dev/api/planets/1/
-	// fetch('http://genesysapi.herokuapp.com/api/talents', { method: 'GET' })
-	//   .then(res => console.log(res));
 	const el = document.getElementById('search-result');
 	el.innerText = 'Fetching...';
 
@@ -12,7 +9,7 @@ async function tryIt() {
 	console.log(category, search);
 
 	try {
-		const result = await fetch(`http://localhost:8080/api/${category}${search}`, { method: 'GET' });
+		const result = await fetch(`http://genesysapi.herokuapp.com/api/${category}${search}`, { method: 'GET' });
 		const data = await result.json();
 		el.innerHTML = `<pre>${JSON.stringify(data, undefined, 2)}</pre>`;
 	} catch (err) {
